8d7570ea062e8c6c363ee4f8f9044c814f76291d
Paul Aguaiza <Paul Aguaiza@169.254.2.231>
2022-07-15 08:11:04 -0500

    terminadoEmpleado

--- src/main/java/ups/ejercicio_04_01/Controller/EmpleadoController.java
+++ src/main/java/ups/ejercicio_04_01/Controller/EmpleadoController.java
@@ -6,6 +6,7 @@ package ups.ejercicio_04_01.Controller;
 
 import java.time.LocalDate;
 import java.util.List;
+import javax.swing.JOptionPane;
 import ups.ejercicio_04_01.Model.Departamento;
 import ups.ejercicio_04_01.Model.Empleado;
 import ups.ejercicio_04_01.Service.DepartamentoService;
@@ -22,12 +23,28 @@ public class EmpleadoController {
     public Empleado crearEmpleado(String cedula, String nombre, int anioNacimiento, int mesNacimiento, int diaNacimiento, 
             String nacionalidad, String direccion, String cargo, float salario, Departamento departamento){
         
-        var fechaNacimiento = LocalDate.of(anioNacimiento, mesNacimiento, diaNacimiento);
+          var fechaNacimiento = LocalDate.of(anioNacimiento, mesNacimiento, diaNacimiento);
         // fecha de entrada = fecha del sistema
         var fechaEntrada = LocalDate.now();
         
+        if (cedula.equals("")) {
+            throw new NullPointerException("Debe ingresar la cedula");
+        }
+        if (nombre.equals("")){
+            throw new NullPointerException("Debe ingresar un nombre");
+        }
+        if (cargo.equals("")){
+            throw new NullPointerException("Debe ingresar un cargo");
+        }
+        
+        if (!isAdult(fechaNacimiento)) {
+            throw new RuntimeException("Empleado debe ser mayor a 18 años");
+        }
         var nuevoEmpleado =  new Empleado(cedula,nombre,fechaNacimiento,nacionalidad,direccion,cargo,salario,fechaEntrada,departamento);
-        return empleadoService.crearEmpleado(nuevoEmpleado);
+        return empleadoService.crearEmpleado(nuevoEmpleado);  
+        
+        
+        
                 
     }
     
@@ -50,7 +67,12 @@ public class EmpleadoController {
     public Empleado eliminarEmpleado(String cedula){
         return empleadoService.eliminarEmpleado(cedula);
     }
-     
+    public boolean isAdult(LocalDate date){
+        var anioActual = LocalDate.now().getYear();
+        var anioNacimiento = date.getYear();
+        var age = anioActual-anioNacimiento;
+        return age>=18;
+    } 
     
     
     
